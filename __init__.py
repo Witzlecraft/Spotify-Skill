0a5f080b6cbef74ecc3d7254c66306ff23862bba
Witzlecraft <91155735+Witzlecraft@users.noreply.github.com>
2022-12-02 23:27:55 +0100

    spotify

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft import intent_file_handler, MycroftSkill, intent_handler
 from mycroft.skills.context import adds_context, removes_context
-
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 class Spotify(MycroftSkill):
     def __init__(self):
@@ -14,8 +15,13 @@ class Spotify(MycroftSkill):
         # Do a blink
         if action.lower() == "blink":
             self.log.info("Blinking! " + action)
-            data = {'inqiury_total': "" + action}
+            data = {'word': "" + action}
             self.speak_dialog('answer', data)
+
+            sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="86b15566a333442d857c62305715ce33", client_secret="c0640cff12c64092abedb334d9678fc8"))
+            results = sp.search(q='weezer', limit=20)
+            for idx, track in enumerate(results['tracks']['items']):
+                print(idx, track['name'])
         
 
 
