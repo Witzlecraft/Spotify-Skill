188b14943bf27372dcd6d423aa2a2190755f9ec6
Witzlecraft <91155735+Witzlecraft@users.noreply.github.com>
2022-12-06 23:47:24 +0100

    f

--- __init__.py
+++ __init__.py
@@ -18,19 +18,49 @@ class Spotify(MycroftSkill):
         self.log.info("Blinking! " + action)
         data = {'word': "" + action}
         self.speak_dialog('answer', data)
+            
 
-        spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="86b15566a333442d857c62305715ce33",
-                                                           client_secret="afd8ce39aa304b6db8fb1901d01b4fec"))
-        if len(sys.argv) > 1:
-            name = ' '.join(sys.argv[1:])
-        else:
-            name = ''+action
 
-        results = spotify.search(q='track:' + name, type='track')
-        items = results['track']['items']
-        if len(items) > 0:
-            track = items[0]
-            print(track['name'], track['preview_url'][0]['url'])
+        music_name = action
+        query_string = urllib.parse.urlencode({"search_query": music_name})
+        formatUrl = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+        search_results = re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
+        clip = requests.get("https://www.youtube.com/watch?v=" + "{}".format(search_results[0]))
+        clip2 = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
+
+        video_url = clip2
+        video_info = youtube_dl.YoutubeDL().extract_info(
+            url = video_url,download=False
+        )
+        filename = f"{video_info['title']}.mp3"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':filename,
+        }
+
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+
+        print("Download complete... {}".format(filename))
+        print("Playing Audio (" + filename + ")...")
+        subprocess.Popen(['omxplayer', '-o', 'hdmi', filename]).wait()
+
+
+
+
+        #spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="86b15566a333442d857c62305715ce33",
+        #                                                   client_secret="afd8ce39aa304b6db8fb1901d01b4fec"))
+        #if len(sys.argv) > 1:
+        #    name = ' '.join(sys.argv[1:])
+        #else:
+        #    name = ''+action
+
+        #results = spotify.search(q='track:' + name, type='track')
+        #items = results['track']['items']
+        #if len(items) > 0:
+        #    track = items[0]
+        #    print(track['name'], track['preview_url'][0]['url'])
         
 
 
